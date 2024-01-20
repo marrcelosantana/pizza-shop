8447db2e39538e0246f9ed8b01dcaaaf4405e088
marrcelosantana <marcelosantana@alu.ufc.br>
2024-01-20 13:52:22 -0300

    Adicionando componente de cards.

--- src/pages/app/orders/components/order-details.tsx
+++ src/pages/app/orders/components/order-details.tsx
@@ -92,6 +92,7 @@ export function OrderDetails() {
               <TableCell className="text-right">R$ 119,80</TableCell>
             </TableRow>
           </TableBody>
+
           <TableFooter>
             <TableRow>
               <TableCell colSpan={3}>Total do pedido</TableCell>
